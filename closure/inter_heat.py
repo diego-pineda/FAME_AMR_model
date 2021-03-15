4bf6beaec94bf9358b8f69795b40403b24bbdd8a
diego-pineda <diegopineda1109@gmail.com>
2021-03-15 10:52:10 +0100

    Added condition to calculate heat transfer coefficient when velocity of HTF is zero. For now, Nu = 5 is used as first approximation

--- closure/inter_heat.py
+++ closure/inter_heat.py
@@ -17,6 +17,10 @@ def beHeff_E(Dsp, Ud, fCp, fK, fMu, fRho, freq, mCp, mK, mRho, er):
 @jit(f8     (f8 , f8,  f8, f8,  f8,   f8,   f8,  f8, f8,   f8, f8),nopython=True)
 def beHeff_I(Dsp, Ud, fCp, fK, fMu, fRho, freq, mCp, mK, mRho, er):
     # Iman uses the DF factor, and a wacou Nu.
-    hefff = (2 + 0.11e1 * (fRho * Ud * Dsp / fMu) ** 0.6e0 * (fMu * fCp / fK) ** (0.1e1 / 0.3e1)) * fK / Dsp / (1 + (2 + 0.11e1 * (fRho * Ud * Dsp / fMu) ** 0.6e0 * (fMu * fCp / fK) ** (0.1e1 / 0.3e1)) * fK / mK * (1 - 0.1e1 / mK * mRho * mCp * freq * Dsp ** 2 / 35) / 10)
-    beta  = 6 * (1 - er) / Dsp
+    if Ud == 0:
+        Nu_f = 5
+        hefff = Nu_f * fK / Dsp
+    else:
+        hefff = (2 + 0.11e1 * (fRho * Ud * Dsp / fMu) ** 0.6e0 * (fMu * fCp / fK) ** (0.1e1 / 0.3e1)) * fK / Dsp / (1 + (2 + 0.11e1 * (fRho * Ud * Dsp / fMu) ** 0.6e0 * (fMu * fCp / fK) ** (0.1e1 / 0.3e1)) * fK / mK * (1 - 0.1e1 / mK * mRho * mCp * freq * Dsp ** 2 / 35) / 10)
+    beta = 6 * (1 - er) / Dsp
     return hefff*beta
