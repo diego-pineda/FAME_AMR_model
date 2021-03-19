c40a6c407e55bc2805fab9a74b53d84f16a61dea
diego-pineda <diegopineda1109@gmail.com>
2021-03-19 11:27:09 +0100

    Added tdma_solver.py to local repository

---
+++ core/tdma_solver.py
@@ -0,0 +1,28 @@
+import numba as nb
+from numba import jit, f8, int32,b1
+# Numpy library
+import numpy as np
+
+@jit(f8[:] (f8[:],f8[:],f8[:],f8[:] ),nopython=True)
+def TDMAsolver(a, b, c, d):
+    '''
+    TDMA solver, a b c d can be NumPy array type or Python list type.
+    refer to http://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+    and to http://www.cfd-online.com/Wiki/Tridiagonal_matrix_algorithm_-_TDMA_(Thomas_algorithm)
+    '''
+    nf = len(d)  # number of equations
+    ac = np.copy(a)
+    bc = np.copy(b)
+    cc = np.copy(c)
+    dc = np.copy(d)
+    for it in range(1, nf):
+        mc = ac[it - 1] / bc[it - 1]
+        bc[it] = bc[it] - mc * cc[it - 1]
+        dc[it] = dc[it] - mc * dc[it - 1]
+    xc = bc
+    xc[-1] = dc[-1] / bc[-1]
+
+    for il in range(nf - 2, -1, -1):
+        xc[il] = (dc[il] - cc[il] * xc[il + 1]) / bc[il]
+
+    return xc
