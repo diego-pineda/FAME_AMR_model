6e24d899845b47b4916c32c924c77472ffb3bdd3
diego-pineda <diegopineda1109@gmail.com>
2021-03-29 11:01:56 +0200

    Added a section to test the functions using the if __name__ == '__main__' command

--- closure/inter_heat.py
+++ closure/inter_heat.py
@@ -31,4 +31,42 @@ def beHeff_I(Dsp, Ud, fCp, fK, fMu, fRho, freq, mCp, mK, mRho, er):
     return hefff*beta
 
 
+if __name__ == '__main__':
 
+    from sourcefiles.fluid.density import fRho
+    from sourcefiles.fluid.dynamic_viscosity import fMu
+    from sourcefiles.fluid.conduction import fK
+    from sourcefiles.fluid.specific_heat import fCp
+
+    T_fluid = 280  # [K] Temperature of fluid
+    perc_gly = 20  # [-] Percentage of glycol in the mixture
+    Dsp = 400e-6  # [m] Diameter of sphere
+    Vol_flow_rate = 30.52e-6  # [m3/s] Volumetric flow rate
+    Area_cross = 0.045*0.013  # [m2] Cross sectional area of bed
+    Porosity = 0.36  # [] Porosity of be
+    fAMR = 1.7  # [Hz]
+    Gd_K = 14  # [W/(m*K)]
+    Gd_rho = 7900  # [kg/m3]
+    Gd_cp = 240  # [J/(kg*K)]
+
+    rho_fluid = fRho(T_fluid, perc_gly)
+    mu_fluid = fMu(T_fluid, perc_gly)
+    cp_fluid = fCp(T_fluid,perc_gly)
+    k_fluid = fK(T_fluid,perc_gly)
+    Ud = Vol_flow_rate/Area_cross
+
+    beta = 6 * (1 - Porosity) / Dsp
+    # Dsp, Ud, fCp, fK, fMu, fRho, freq, mCp, mK, mRho, er
+    hI = beHeff_I(Dsp, Ud, cp_fluid, k_fluid, mu_fluid, rho_fluid, fAMR, Gd_cp, Gd_K, Gd_rho, Porosity)/beta
+    hE = beHeff_E(Dsp, Ud, cp_fluid, k_fluid, mu_fluid, rho_fluid, fAMR, Gd_cp, Gd_K, Gd_rho, Porosity)/beta
+
+    # Heat transfer coefficient calculated by using the correlation presented in the book of Mills
+
+    Vel_char = Ud / Porosity
+    Len_char = Dsp * (Porosity / (1 - Porosity))
+    Re = Vel_char * Len_char * rho_fluid / mu_fluid
+    Pr = cp_fluid * mu_fluid / k_fluid
+    Nu = (0.5 * Re ** 0.5 + 0.2 * Re ** (0.2e1/0.3e1)) * Pr ** (0.1e1 / 0.3e1)
+    hM = Nu * k_fluid / Len_char
+
+    print("hE = {} [W/(m2*K)], hI = {} [W/(m2*K)], hM = {} [W/(m2*K)]".format(hE, hI, hM))
