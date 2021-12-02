2109e5bdc7626eb1437ba3e82c646639fb4bdf10
diego-pineda <diegopineda1109@gmail.com>
2021-12-02 23:54:20 +0100

    Run_parallel.py took parameters that are common to all simulated cases out of the def RunCaseThotTcold(case, jobName) function. Moved the position of if __name__ == __main__ statement. Added import of configuration file to allow modification of parameters from the input file

--- Run_parallel.py
+++ Run_parallel.py
@@ -1,44 +1,121 @@
 from FAME_DP_V1 import runActive
 import numpy as np
 import sys
+from configurations import R8
+
+# ------- Some useful functions for storing data --------
+
+def FileSave(filename, content):
+    with open(filename, "a") as myfile:
+        myfile.write(content)
+
+
+def FileSaveMatrix(filename, content):
+    with open(filename, "a") as f:
+        for line in content:
+            f.write(" ".join("{:9.6f}\t".format(x) for x in line))
+            f.write("\n")
+
+
+def FileSaveVector(filename, content):
+    with open(filename, "a") as f:
+        f.write(" ".join("{:9.6f}\t".format(x) for x in content))
+        f.write("\n")
+
+
+# ------- Definition of main variables of the cases to simulate -------
+
+'''Note: In principle any variable could become x in xResolution, e.g. dispV. It does not mean that
+the name xResolution needs to be adjusted. hotResolution must be 1 when xResolution is different than 1, but they both
+can be 1 simultaneously. For each case to simulate, the value of the variable that is going to take the place of x, 
+must be defined in the same way Thot is defined, i.e. by using the following equation (taking as an example the 
+variable dispV): dispV = xArr[int(np.floor(case/coldResolution)%xResolution)]. This variable has to be defined outside 
+the if conditions (if casenum == 0, 1, 2, etc).'''
+
+numCases       = 6
+hotResolution  = 1
+coldResolution = 6
+xResolution    = 1
+
+Thotarr = np.linspace(273+27, 273+27, hotResolution)
+xArr    = []
+maxcase = numCases * hotResolution * coldResolution * xResolution
+
+# ------- Input parameters common to all cases -------
+
+# Numerical parameters
+nodes         = 400
+timesteps     = 600
+time_limit    = 600  # [min] Time limit for the simulation in minutes
+cycle_toler   = 1e-5  # Maximum cycle tolerance: criterion for ending the iterative calculation process
+maxStepIter   = 500  # Maximum time step iterations the simulation is allowed to take
+maxCycleIter  = 500  # Maximum cycle iterations the simulation is allowed to take
+cen_loc       = 0
+
+# Temperatures
+MaxTSpan      = 30
+Tambset       = 300
+
+# Frequency of AMR cycle
+# ff            = 0.25
+
+# Flow profile
+
+# - FAME cooler
+dispV         = 30.52e-6
+acc_period    = 10
+max_flow_per  = 45
+full_magn_ang = 30
+unbal_rat     = 1
+from sourcefiles.device.FAME_V_flow import vol_flow_rate
+volum_flow_profile = vol_flow_rate(timesteps, dispV, acc_period, max_flow_per, full_magn_ang, unbal_rat)
+
+# - POLO cooler
+# dispV = 4.74e-6  # [m3/s] DP: device vol. flow rate = 1.84 L/min, 2 regenerators with simultaneous flow.
+# from sourcefiles.device.polo_V_flow import polo_vol_flow
+# volum_flow_profile = polo_vol_flow(timesteps, dispV, ff)
+
+# Magnetic field profile
+
+# - FAME cooler
+from sourcefiles.device import FAME_app_field
+app_field = FAME_app_field.app_field(timesteps, nodes)
+
+# - POLO cooler
+# from sourcefiles.device.polo_mag_field import polo_app_field
+# app_field = polo_app_field(timesteps, nodes, 0.1)
+
+# Geometric parameters
+'''Geometric parameters that differ from the configuration from case to case can be adjusted here. This way it is not
+necessary to create a new configuration file each time a single parameter need to be changed. Take the following lines
+starting with R8 as an example.'''
+
+# R8.species_discription = ['reg-M6', 'reg-M7', 'reg-M8', 'reg-M9', 'reg-M10', 'reg-M11', 'reg-M12', 'reg-M13', 'reg-M14', 'reg-M15']
+# R8.x_discription = [0, 0.006, 0.012, 0.018, 0.024, 0.030, 0.036, 0.042, 0.048, 0.054, 0.060]
+# R8.reduct_coeff = dict(M0=1, M1=0.55, M2=0.77, M6=1, M7=1, M8=1, M9=1, M10=1, M11=1, M12=1, M13=1, M14=1, M15=1)
+# R8.mK = 6
+# R8.mRho = 6100
+cName = "R8"
+num_reg = 1
+
+# Switches for activating and deactivating terms in governing equations
+CF   = 1
+CS   = 1
+CL   = 1
+CVD  = 1
+CMCE = 1
+
+# Heat transfer models
+htc_model_name = 'wakao_and_kagei_1982'  # Name of the file containing the function of the model for htc
+leaks_model_name = 'polo_resistance'  # Name of the file containing the function of the model for heat leaks
 
 
 if __name__ == '__main__':
 
-    # ------- Some useful functions for storing data --------
-
-    def FileSave(filename, content):
-        with open(filename, "a") as myfile:
-            myfile.write(content)
-
-    def FileSaveMatrix(filename, content):
-        with open(filename, "a") as f:
-            for line in content:
-                f.write(" ".join("{:9.6f}\t".format(x) for x in line))
-                f.write("\n")
-
-    def FileSaveVector(filename, content):
-        with open(filename, "a") as f:
-            f.write(" ".join("{:9.6f}\t".format(x) for x in content))
-            f.write("\n")
-
     def RunCaseThotTcold(case, jobName):  # DP: this is necessary for running arrays of tasks in the cluster
 
-        numCases       = 6
-        hotResolution  = 1
-        coldResolution = 6
-        xResolution    = 1
-        # Note: In principle any variable could become x in xResolution, e.g. dispV. It does not mean that
-        # the name xResolution needs to be adjusted. hotResolution must be 1 when xResolution is different than 1.
-        # For each case to simulate, the value of the variable that is going to take the place of x, must be defined in
-        # the same way Thot is defined, i.e. by using the following equation taking as an example the variable dispV:
-        # dispV = xArr[int(np.floor(case/coldResolution)%xResolution)]. This variable has to be defined outside the if
-        # conditions, if casenum == 0, 1, 2, etc.
-
-        maxcase = numCases * hotResolution * coldResolution * xResolution
-        Thotarr = np.linspace(273+27, 273+27, hotResolution)
-        xArr    = []
-        casenum=int(np.floor(case/(hotResolution*coldResolution)))
+        casenum  = int(np.floor(case/(hotResolution*coldResolution)))
+        fileName = "{}.txt".format(jobName)
 
         # ------- Parameters that change for the cases to study -------
 
@@ -71,68 +148,6 @@ if __name__ == '__main__':
             # Frequency of AMR cycle
             ff = 1.5
 
-        # ------- Input parameters common to all cases -------
-
-        fileName      = "{}.txt".format(jobName)
-
-        # Numerical parameters
-        nodes         = 400
-        timesteps     = 600
-        time_limit    = 600  # [min] Time limit for the simulation in minutes
-        cycle_toler   = 1e-5  # Maximum cycle tolerance: criterion for ending the iterative calculation process
-        maxStepIter   = 500  # Maximum time step iterations the simulation is allowed to take
-        maxCycleIter  = 500  # Maximum cycle iterations the simulation is allowed to take
-        cen_loc       = 0
-
-        # Temperatures
-        MaxTSpan      = 30
-        Tambset       = 300
-
-        # Frequency of AMR cycle
-        # ff            = 0.25
-
-        # Flow profile
-
-        # - FAME cooler
-        # dispV         = 30.52e-6
-        # acc_period    = 10
-        # max_flow_per  = 45
-        # full_magn_ang = 30
-        # unbal_rat     = 1
-        # from sourcefiles.device.FAME_V_flow import vol_flow_rate
-        # volum_flow_profile = vol_flow_rate(timesteps, dispV, acc_period, max_flow_per, full_magn_ang, unbal_rat)
-
-        # - POLO cooler
-        dispV = 4.74e-6  # [m3/s] DP: device vol. flow rate = 1.84 L/min, 2 regenerators with simultaneous flow.
-        from sourcefiles.device.polo_V_flow import polo_vol_flow
-        volum_flow_profile = polo_vol_flow(timesteps, dispV, ff)
-
-        # Magnetic field profile
-
-        # - FAME cooler
-        # from sourcefiles.device import FAME_app_field
-        # app_field = FAME_app_field.app_field(timesteps, nodes)
-
-        # - POLO cooler
-        from sourcefiles.device.polo_mag_field import polo_app_field
-        app_field = polo_app_field(timesteps, nodes, 0.1)
-
-        # Geometric parameters
-        cName = "polo_1"
-        num_reg = 1
-
-        # Switches for activating and deactivating terms in governing equations
-        CF   = 1
-        CS   = 1
-        CL   = 1
-        CVD  = 1
-        CMCE = 1
-
-        # Heat transfer models
-        htc_model_name = 'wakao_and_kagei_1982'  # Name of the file containing the function of the model for htc
-        leaks_model_name = 'polo_resistance'  # Name of the file containing the function of the model for heat leaks
-
-
         # -------- Setting values of Thot and Tcold for simulation -------
 
         Thot = Thotarr[int(np.floor(case/coldResolution)%hotResolution)]
@@ -151,7 +166,7 @@ if __name__ == '__main__':
         #  13     14 15 16    17       18  19   20 21    22         23       24         25      26     27
 
         fileNameSave = './output/' + str(case) + fileName
-        FileSave(fileNameSave, "{},{},{},{},{},{},{},{} \n".format('Tspan [K]', 'Qh [W]', 'Qc [W]', 'Cycles [-]', 'run time [min]', 'Max. Pressure drop [Pa]','Thot [K]', 'Tcold [K]'))
+        FileSave(fileNameSave, "{},{},{},{},{},{},{},{} \n".format('Tspan [K]', 'Qh [W]', 'Qc [W]', 'Cycles [-]', 'run time [min]', 'Max. Pressure drop [Pa]', 'Thot [K]', 'Tcold [K]'))
         FileSave(fileNameSave, "{},{:4.2f},{:4.2f},{},{:4.2f},{:4.2f},{},{} \n".format(results[0]-results[1], results[26], results[2], results[27], results[4], results[17], Thot, Tcold))
         FileSave(fileNameSave, "Fluid temperatures\n")
         FileSaveMatrix(fileNameSave, results[14])
@@ -159,18 +174,5 @@ if __name__ == '__main__':
         FileSaveMatrix(fileNameSave, results[15])
         FileSave(fileNameSave, "Pressure drop accross the regenerator for the entire cycle\n")
         FileSaveVector(fileNameSave, results[16])
-
-        # fileNameSave        = './' + fileName # DP: ./ is for specifying that the file is save to the working directory
-        # fileNameEndTemp     = './Ends/{:3.0f}-{:3.0f}-PysicalEnd'.format(Thot,Tcold)+fileName
-        # fileNameSliceTemp   = './Blow/{:3.0f}-{:3.0f}-BlowSlice'.format(Thot,Tcold)+fileName
-        # FileSave(fileNameSave,"{},{},{},{},{},{},{} \n".format(results[0],results[1],results[2],results[3],results[4],results[5],results[26]) )
-        # #FileSave(fileNameEndTemp,"{},{},{},{},{} \n".format('Thot [K]', 'Tcold [K]','Uti [-]', 'freq [Hz]', 'run time [min]','Eff CE-HB [-]', 'Eff HE-CB [-]') )
-        # #FileSave(fileNameEndTemp,"{},{},{},{},{} \n".format(results[0],results[1],results[18],results[19], results[4],results[6],results[7]) )
-        # #EndTemperatures = np.stack((results[20], results[8],results[9]), axis=-1)
-        # #FileSaveMatrix(fileNameEndTemp,EndTemperatures)
-        # FileSave(fileNameSliceTemp,"{},{},{},{},{} \n".format('Thot [K]', 'Tcold [K]','Uti [-]', 'freq [Hz]', 'run time [min]') )
-        # FileSave(fileNameSliceTemp,"{},{},{},{},{} \n".format(results[0],results[1],results[18],results[19], results[4]) )
-        # BlowSliceTemperatures = np.stack((results[21],results[10],results[11],results[12],results[13],results[22],results[23],results[24],results[25]), axis=-1)
-        # FileSaveMatrix(fileNameSliceTemp,BlowSliceTemperatures)
 
     RunCaseThotTcold(float(sys.argv[1]), sys.argv[2])
