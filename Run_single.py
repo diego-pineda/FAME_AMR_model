bc96dc15ab98162cc5fc632272964b0abf7e9f94
diego-pineda <diegopineda1109@gmail.com>
2022-03-29 17:18:49 +0200

    Run_single.py included function for reducing size of matrices

--- Run_single.py
+++ Run_single.py
@@ -8,18 +8,18 @@ import numpy as np
 caseNumber    = 1
 
 # Numerical parameters
-nodes         = 300
+nodes         = 1800
 timesteps     = 400
 time_limit    = 600  # [min] Time limit for the simulation in minutes
-cycle_toler   = 1e-1  # Maximum cycle tolerance: criterion for ending the iterative calculation process
+cycle_toler   = 1e-4  # Maximum cycle tolerance: criterion for ending the iterative calculation process
 maxStepIter   = 2000  # Maximum time step iterations the simulation is allowed to take
 maxCycleIter  = 1000  # Maximum cycle iterations the simulation is allowed to take
 cen_loc       = 0
 
 # Simulation temperatures
-Thot          = 312
-Tcold         = 298
-Tambset       = 300
+Thot          = 307
+Tcold         = 280
+Tambset       = 288
 
 # Frequency of AMR cycle
 ff            = 1  # [Hz] frequency of AMR cycle
@@ -27,7 +27,7 @@ ff            = 1  # [Hz] frequency of AMR cycle
 # Flow profile
 
 # FAME cooler
-dispV         = 1.1 * 16.667e-6  # [m3/s] DP: device vol. flow rate = 1.84 L/min, 2 regenerators with simultaneous flow.
+dispV         = 2.5 * 16.667e-6  # [m3/s] DP: device vol. flow rate = 1.84 L/min, 2 regenerators with simultaneous flow.
 acc_period    = 5
 max_flow_per  = 45
 full_magn_ang = 30
@@ -56,13 +56,14 @@ app_field = FAME_app_field.app_field(timesteps, nodes, mag_field)
 necessary to create a new configuration file each time a single parameter need to be changed. Take the following lines
 starting with R8 as an example.'''
 from configurations import R8
-R8.species_discription = ['reg-M6', 'reg-M7', 'reg-M8', 'reg-M9', 'reg-M10', 'reg-M11', 'reg-M12', 'reg-M13', 'reg-M14', 'reg-M15']
-R8.x_discription = [0, 0.006, 0.012, 0.018, 0.024, 0.030, 0.036, 0.042, 0.048, 0.054, 0.060]
-R8.reduct_coeff = dict(M0=1, M1=0.55, M2=0.77, M6=1, M7=1, M8=1, M9=1, M10=1, M11=1, M12=1, M13=1, M14=1, M15=1)
+R8.species_discription = ['reg-M166', 'reg-M167', 'reg-M168', 'reg-M169', 'reg-M170', 'reg-M171', 'reg-M172', 'reg-M173', 'reg-M174', 'reg-M175', 'reg-M176', 'reg-M177', 'reg-M178', 'reg-M179']
+R8.x_discription = [0, 0.00428571, 0.00857143, 0.01285714, 0.01714286, 0.02142857, 0.02571429, 0.03, 0.03428571, 0.03857143, 0.04285714, 0.04714286, 0.05142857, 0.05571429, 0.06]
+R8.reduct_coeff = dict(M166=0.77, M167=0.77, M168=0.77, M169=0.77,
+                       M170=0.77, M171=0.77, M172=0.77, M173=0.77, M174=0.77, M175=0.77, M176=0.77, M177=0.77, M178=0.77, M179=0.77)
 R8.mK = 6
 R8.mRho = 6100
 cName   = "R8"  # Name of file where the geometric configuration of the regenerator is defined
-jName   = "Test_write_new_matrices"  # DP: use underlines to connect words because this is used as file name
+jName   = "Test_change_cp_calc"  # DP: use underlines to connect words because this is used as file name
 num_reg = 1
 
 # Switches for activating and deactivating terms in governing equations
@@ -82,7 +83,7 @@ results = runActive(caseNumber, Thot, Tcold, cen_loc, Tambset, ff, CF, CS, CL, C
                     htc_model_name, leaks_model_name, num_reg)
 
 
-# Some useful functions for storing data.
+# ------------------ Some useful functions for storing data --------------------
 def FileSave(filename, content):
     with open(filename, "a") as myfile:
         myfile.write(content)
@@ -100,6 +101,22 @@ def FileSaveVector(filename, content):
         f.write(" ".join("{:9.6f}\t".format(x) for x in content))
         f.write("\n")
 
+# ------------------- Function for reducing the size of matrices --------------------
+
+def reduce_matrix(nodes, timesteps, matrix, fraction_nodes, fraction_timesteps):
+    new_matrix = np.ones((int(timesteps / fraction_timesteps)+1, int(nodes / fraction_nodes)+1))
+    ii = 0
+    for i in range(timesteps+1):
+        jj = 0
+        if i % fraction_timesteps == 0:
+            for j in range(nodes+1):
+                if j % fraction_nodes == 0:
+                    new_matrix[ii, jj] = matrix[i, j]
+                    jj = jj + 1
+            ii = ii + 1
+    return new_matrix
+
+
 #  runActive():  returns
 # Thot          0   eff_HB_CE   6   sHalfBlow   12  Uti         18  sMaxCBlow   24  fluid_dens  30
 # Tcold         1   eff_CB_HE   7   sEndBlow    13  freq        19  sMaxHBlow   25  mass_flow   31
@@ -109,25 +126,25 @@ def FileSaveVector(filename, content):
 # pave          5   yEndBlow    11  np.max(pt)  17  yMaxHBlow   23  htc_fs      29
 
 
-fileName = "Test_write_new_matrices.txt"
+fileName = "Test_change_cp_calc.txt"
 fileNameSave = './output/' + fileName
 #FileSave(fileNameSave,"{},{},{},{},{},{},{} \n".format(results[0], results[1], results[2], results[3], results[4], results[5],results[26]))
 FileSave(fileNameSave, "{},{},{},{},{},{} \n".format('Tspan [K]', 'Qh [W]', 'Qc [W]', 'Cycles [-]', 'run time [min]', 'Max. Pressure drop [Pa]'))
 FileSave(fileNameSave, "{},{:4.2f},{:4.2f},{},{:4.2f},{:4.2f} \n".format(results[0]-results[1], results[26], results[2], results[27], results[4], results[17]))
 FileSave(fileNameSave, "Fluid temperatures\n")
-FileSaveMatrix(fileNameSave, results[14])
+FileSaveMatrix(fileNameSave, reduce_matrix(nodes, timesteps, results[14], 3, 2))
 #FileSave(fileNameSave, "\n")
 FileSave(fileNameSave, "Solid temperatures\n")
-FileSaveMatrix(fileNameSave, results[15])
+FileSaveMatrix(fileNameSave, reduce_matrix(nodes, timesteps, results[15], 3, 2))
 #FileSave(fileNameSave, "\n")
 FileSave(fileNameSave, "Pressure drop accross the regenerator for the entire cycle\n")
 FileSaveVector(fileNameSave, results[16])
 FileSave(fileNameSave, "\nInternal Magnetic Field\n")
-FileSaveMatrix(fileNameSave, results[28])
+FileSaveMatrix(fileNameSave, reduce_matrix(nodes, timesteps, results[28], 3, 2))
 FileSave(fileNameSave, "\nHeat transfer coefficient between solid and fluid in the packed bed\n")
-FileSaveMatrix(fileNameSave, results[29])
+FileSaveMatrix(fileNameSave, reduce_matrix(nodes, timesteps, results[29], 3, 2))
 FileSave(fileNameSave, "\nMass flow rate\n")
-FileSaveMatrix(fileNameSave, results[31])
+FileSaveMatrix(fileNameSave, reduce_matrix(nodes, timesteps, results[31], 3, 2))
 
 #FileSave(fileNameSave, "\n")
 
