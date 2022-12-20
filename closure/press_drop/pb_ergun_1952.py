b97368bfa1a90988270994ab409c5d7d80d91794
diego-pineda <diegopineda1109@gmail.com>
2022-12-20 13:40:37 +0100

    refactor(pb_ergun_1952.py): snapshot of a working code before adding macroscopic inertial force to pressure drop calculation

--- closure/press_drop/pb_ergun_1952.py
+++ closure/press_drop/pb_ergun_1952.py
@@ -8,5 +8,5 @@ import numpy as np
 def SPresM(Dsp, Ud, V, er, flMu, flRho, Af):
     # The following corresponds to Ergun's correlation for calculation of pressure drop as presented in Mills
     dP = (1.75 * Ud ** 2 * (1 - er) / Dsp / er ** 3 * flRho + 150 * Ud * (1 - er) ** 2 / Dsp ** 2 / er ** 3 * flMu)
-    Spress = dP * V
+    Spress = dP * V  # dP is actually dP/dx, i.e. pressure drop per unit length
     return Spress, dP
