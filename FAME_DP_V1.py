fix(runActive(): changes position of calculation of magnetic power to the inside of the cycle loop)

FAME_DP_V1.py
@@ -1350,6 +1350,19 @@ def runActive(caseNum, Thot, Tcold, cen_loc, Tambset, ff, CF, CS, CL, CVD, CMCE,
             # Quit Program
             sys.exit()
 
+        # ----------------------------- 27/10/2022 Calculation of magnetic power input -------------------------------------
+        Ts = s * (Thot - Tcold) + Tcold
+        P_mag_AMR = 0
+        for i in range(N):  # Ghost nodes excluded from this calculation
+            ms_h = S_h_if_list[materials.index(species_descriptor[i+1])]
+            P_mag_node = 0
+            for n in range(nt):  # Ghost nodes excluded from this calculation
+                s_current = ms_h(Ts[n, i+1], int_field[n, i+1])[0, 0]
+                s_next = ms_h(Ts[n+1, i+1], int_field[n+1, i+1])[0, 0]
+                P_mag_node = P_mag_node + freq * mRho * (W_reg*H_reg*DX*(1-e_r[i+1])) * 0.5 * (Ts[n, i+1] + Ts[n+1, i+1]) * (s_next - s_current)  # [W] Magnetic power over the full cycle for the current node
+            P_mag_AMR = P_mag_AMR + P_mag_node  # [W] Magnetic power over the entire AMR for the full cycle
+        # ------------------------------------------------------------------------------------------------------------------
+
         # Copy last value to the first of the next cycle.
         if cycleCount % 2 == 0:  # Convergence accelation implemented. Reference: Int J Refrig. 65 (2016) 250-257
             cycleTol = bool_y_check and bool_s_check  # DP comment: this will return True if both arguments are True, otherwise it returns False, which will keep the cycle while loop running
@@ -1534,22 +1547,9 @@ def runActive(caseNum, Thot, Tcold, cen_loc, Tambset, ff, CF, CS, CL, CVD, CMCE,
             S_condu_stat = S_condu_stat + freq * k_stat[j, i] * Ac * (1 - e_r[i]) * dTsdx**2 * DX * DT / Ts[j, i]**2
             S_condu_disp = S_condu_disp + freq * k_disp[j, i] * Ac * e_r[i] * dTfdx**2 * DX * DT / Tf[j, i]**2
 
-    # ----------------------------- 27/10/2022 Calculation of magnetic power input -------------------------------------
-
-    P_mag_AMR = 0
-    for i in range(N):  # Ghost nodes excluded from this calculation
-        ms_h = S_h_if_list[materials.index(species_descriptor[i+1])]
-        P_mag_node = 0
-        for n in range(nt):  # Ghost nodes excluded from this calculation
-            s_current = ms_h(Ts[n, i+1], int_field[n, i+1])[0, 0]
-            s_next = ms_h(Ts[n+1, i+1], int_field[n+1, i+1])[0, 0]
-            P_mag_node = P_mag_node + freq * mRho * (W_reg*H_reg*DX*(1-e_r[i+1])) * 0.5 * (Ts[n, i+1] + Ts[n+1, i+1]) * (s_next - s_current)  # [W] Magnetic power over the full cycle for the current node
-        P_mag_AMR = P_mag_AMR + P_mag_node  # [W] Magnetic power over the entire AMR for the full cycle
 
     print('Total pumping power is: {} [W]'.format(P_pump_AMR))
     print('Total magnetic power is: {} [W]'.format(P_mag_AMR))
-
-    # ------------------------------------------------------------------------------------------------------------------
 
     return Thot, Tcold, qc, qccor, (t1-t0)/60, pave, eff_HB_CE, eff_CB_HE, tFce, tFhe, yHalfBlow, yEndBlow, sHalfBlow, \
            sEndBlow, y, s, pt, np.max(pt), Uti, freq, t, xloc, yMaxCBlow, yMaxHBlow, sMaxCBlow, sMaxHBlow, qh, \