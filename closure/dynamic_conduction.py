f8d0573ea5d4ff73550f7554cddcd0fc8a2009d0
diego-pineda <diegopineda1109@gmail.com>
2023-04-05 10:25:55 +0200

    docs(dynamic_conduction.py): adds a note about how Peclet number should be calculated

--- closure/dynamic_conduction.py
+++ closure/dynamic_conduction.py
@@ -7,6 +7,7 @@ import numpy as np
 # Dynamic conduction in the fluid based on Paulo's work
 @jit(f8 (   f8, f8,  f8, f8,   f8, f8),nopython=True)
 def kDyn_P(Dsp, er, fCp, fK, fRho, Ud):
+    # DP: 28/03/2023 Pe should be calculated based on pore velocity and particle radius
     PeNum = fCp / fK * np.abs(Ud) * Dsp * fRho
     if PeNum < 0.01 :
         kd = fK + fRho ** 2 * fCp ** 2 / fK * np.sqrt(0.2e1) * (np.abs(Ud)**2) / (er** 2) * Dsp**2 * ((1 - er) ** (-0.1e1 / 0.2e1)) / 0.240e3
